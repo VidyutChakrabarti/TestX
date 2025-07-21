42f04b761d6be77d2e50af58023660de797ab585
VidyutChakrabarti <chakrabartivr@rknec.edu>
2025-07-21 22:25:51 +0530

    changed header files to match cpp implementation

--- data-structures/Graph/graphtg.h
+++ data-structures/Graph/graphtg.h
@@ -4,6 +4,7 @@
 #include "testgenerator.h"
 #include "graph.h"
 #include <string>
+#include <set>
 
 using namespace std;
 using namespace testgenerator;
@@ -13,13 +14,17 @@ class Graphtg : public TestGenerator<Graph *>
 protected:
     virtual void callPUT(Graph *) override;
     virtual float getCoverage() override;
+    set<int> getCoveredLinesFromGcov();
+    float getIndividualCoverage();
+    float getTotalCoverage();
+
 public:
     static const unsigned int MAXTESTCOUNT;
     unsigned int count = 0;
     Graphtg();
     virtual ~Graphtg();
     virtual Graph *generateRandomTest() override;
-    virtual vector<Graph*> generateTest(Graph* base) override;
+    virtual vector<Graph *> generateTest(Graph *base) override;
     virtual void deleteTest(Graph *);
     virtual bool toContinue() override;
     virtual string toString(Graph *);
@@ -28,7 +33,7 @@ public:
     virtual void writeToFile(string s, string fname);
     virtual void deleteDirectoryContents(const std::filesystem::path &dir);
     virtual void writeAllTestsToFile();
-    virtual Graph* deepCopy(Graph* g) override;
+    virtual Graph *deepCopy(Graph *g) override;
 };
 
 #endif
